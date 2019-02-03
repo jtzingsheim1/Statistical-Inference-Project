3b8c71b475bfe981d9f1e0be6b91b4accb595b4e
jtzingsheim1 <43869157+jtzingsheim1@users.noreply.github.com>
2019-02-03 16:13:44 -0600

    Updated project script with basic instructions

---
+++ C6_Project1.R
@@ -0,0 +1,30 @@
+# Coursera Data Science Specialization Course 6 Project 1 Script
+
+
+# The purpose of this script is to satisfy the requirements of the project 1
+# peer-graded assignment that is part of the Statistical Inference course
+# within the Data Science Specialization on Coursera. As per the instructions
+# the project will consist of the two parts outlined below.
+#
+# The first part is a simulation exercise which will simulate 1000 averages of
+# 40 exponentials and then:
+# 1. Show the sample mean and compare it to the theoretical mean of the
+# distribution
+# 2. Show how variable the sample is (via variance) and compare it to the
+# theoretical variance of the distribution
+# 3. Show that the distribution is approximately normal
+#
+# The second part focuses on basic inferential data analysis. The steps are:
+# 1. Load the ToothGrowth data and perform some basic exploratory data analysis
+# 2. Provide a basic summary of the data
+# 3. Use confidence intervals and/or hypothesis tests to compare tooth growth by
+# supp and dose.
+# 4. State your conclusions and the assumptions needed for your conclusions
+
+
+#library(dplyr)
+
+
+# Part 1) Simulation Exercise
+
+
