b21f8877a66d66449dc3c1edcdcd0f9149dc3336
jtzingsheim1 <43869157+jtzingsheim1@users.noreply.github.com>
2019-02-09 17:12:43 -0600

    Updated part 1 to include Q-Q plots to evaluate normality

--- C6_Project1.R
+++ C6_Project1.R
@@ -62,14 +62,20 @@ theoretical.variance <- 1/(n * lambda^2)  # 0.625
 # The sample variance of 0.598 is fairly close to the theoretical variance
 
 # 3a. Show that the distribution is approximately normal
+par(mfrow = c(1, 2))  # Setup plot space
 hist(sim.results, freq = FALSE)  # Plot the simulation results by probability
 # Next a line representing a normal distribution will be overlaid on this plot
 curve(dnorm(x, mean = theoretical.mean, sd = sqrt(theoretical.variance)),
       add = TRUE)
 # curve() plots a function of x. dnorm() is the expression for the normal
 # distribution, and here it is written as a function of x. dnorm() is scaled to
-# the theoretical mean and standard deviation of this situation. In the plot it
-# is easy to see that the simulation closely follows a normal distribution.
+# the theoretical mean and standard deviation of this situation. In this first
+# plot the histogram appears to follow the reference line well, but it is
+# recommended to check the Q-Q plot as well.
+qqnorm(sim.results, main = "Q-Q Plot of sim.results")  # Add Q-Q Plot
+qqline(sim.results)  # Add reference line
+# In the second plot the simulation results still appear to follow the normal
+# reference line fairly well.
 
 # 3b. Show the effect of the Central Limit Theorem
 
@@ -77,12 +83,16 @@ curve(dnorm(x, mean = theoretical.mean, sd = sqrt(theoretical.variance)),
 sim.results2 <- replicate(1000, rexp(1, lambda))  # Do 1000 sims of 1 obs
 theoretical.variance2 <- 1/(lambda^2)  # 25
 
-# Plot the simulation results by probability
-hist(sim.results2, freq = FALSE)
+# Plot the new simulation results as before
+par(mfrow = c(1, 2))  # Setup plot space
+hist(sim.results2, freq = FALSE)  # Plot the simulation resuls by probability
 curve(dnorm(x, mean = theoretical.mean, sd = sqrt(theoretical.variance2)),
       add = TRUE)  # Add the corresponding normal curve
-# Clearly the simulation will follow the underlying distribution (exponential),
-# so the normal curve does not fit as well in this plot, and the variance is
-# much higher. This shows the power of the CLT, that by taking the mean and
-# taking large samples the distribution becomes more normal.
+qqnorm(sim.results2, main = "Q-Q Plot of sim.results2")
+qqline(sim.results2)
+# These simulation results follow the underlying distribution (exponential), so
+# the normal curve does not fit as well in the first plot. The Q-Q plot makes it
+# much more obvious that the distribution is not normal. This shows the power of
+# the CLT, that by taking the mean and taking large samples the distribution
+# becomes more normal.
 
