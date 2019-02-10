bf01e8eee9d669775c014eedc6ca7445245f0247
jtzingsheim1 <43869157+jtzingsheim1@users.noreply.github.com>
2019-02-09 18:17:11 -0600

    Split the script into two files because the assignment wants two reports

---
+++ C6_Project1b.R
@@ -0,0 +1,21 @@
+# Coursera Data Science Specialization Course 6 Project 1 Part 2 Script
+
+
+# The purpose of this script is to satisfy the requirements of the project 1
+# peer-graded assignment that is part of the Statistical Inference course
+# within the Data Science Specialization on Coursera. As per the instructions
+# the project will consist of the two parts and this script is for part 2.
+#
+# The second part focuses on basic inferential data analysis. The steps are:
+# 1. Load the ToothGrowth data and perform some basic exploratory data analysis
+# 2. Provide a basic summary of the data
+# 3. Use confidence intervals and/or hypothesis tests to compare tooth growth by
+# supp and dose.
+# 4. State your conclusions and the assumptions needed for your conclusions
+
+
+#library(dplyr)
+
+
+# Part 2) Basic Inferential data analysis
+
